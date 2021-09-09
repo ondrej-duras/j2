0eae7e68cb21f3797da2f25a9cb3a2dd94f62c96
Ing. Ondrej ĎURÁS (dury) <ondrej-duras@users.noreply.github.com>
2021-09-09 02:32:59 +0200

    template_j2 + config.json => config_ticket.txt

---
+++ j2json.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python2
+# 20210908, Ing. Ondrej DURAS (dury)
+# -*- coding: ascii -*-
+
+## MANUAL ############################################################# {{{ 1
+
+VERSION = "2021.090801"
+MANUAL  = """
+NAME: J2JSON Jinja2 Template + JSON Config = Configuration Ticket
+FILE: j2json.py
+
+DESCRIPTION:
+  Mandatory Jinja2 template for ticket preparation ?
+  None Ansible around ?
+  This should help.
+
+  Script needs two things on input:
+   - simple Jinja2 template
+   - json configuration
+
+
+USAGE:
+  j2json.py -t TEMPLATE.j2 -c CONFIG.json -o CONFIG-TICKET.txt
+  j2json.py -t TEMPLATE.j2 -c CONFIG.json -s site-x -o CONFIG-TICKET.txt
+
+
+PARAMETERS:
+    -t --template - j2 file containing the template
+    -c --config   - json file with configuration parameters
+    -s --sub      - sub-configuration / part-of-configuration
+    -o --output   - output file with prepared ticket
+    -l --list     - list sub-configuration options
+
+SEE ALSO:
+  https://github.com/ondrej-duras/
+
+VERSION: %s GPLv2
+""" % (VERSION)
+
+
+####################################################################### }}} 1
+## GLOBALs ############################################################ {{{ 1
+
+import sys
+import json
+
+ACTION   = []  # list of actions
+TEMPLATE = ""  # template
+CONFIG   = {}  # configuration
+SUBCFG   = ""  # name of sub-configuration if used
+OUTFILE  = ""  # output file if used (""=stdout by default)
+
+####################################################################### }}} 1
+## LIBRARY ############################################################ {{{ 1
+
+def prepareConfig(template,config):
+  output=template
+  for item in config.keys():
+    value=config[item]
+    if isinstance(value,dict):
+      print("Error: Hierarchycal configuration ( -s need to be used).")
+      print("Error: Item '%s' is subconfig." % (item))
+      exit()
+    output=output.replace(item,value)
+  return output
+
+
+def listSubConfig(config):
+  for item in config.keys():
+    if isinstance(config[item],dict):
+      if item == "common":
+        continue
+      print(item)
+
+
+def mergeDict(x,y):
+   z = x.copy()
+   z.update(y)
+   return z
+
+
+def loadJson(fname):
+  fh=open(fname,"r")
+  output = json.load(fh)
+  fh.close()
+  return output
+
+
+def loadFile(fname):
+  fh=open(fname,"r")
+  output=fh.read()
+  fh.close()
+  return output
+
+
+def writeFile(fname,output):
+  if not fname:
+    print(output)
+  else:
+    fh=open(fname,"w")
+    fh.write(output)
+    fh.close()
+    print("written to '%s'." % (fname))
+  return
+
+####################################################################### }}} 1
+## ACTIONS ############################################################ {{{ 1
+
+def takeAction():
+  global ACTION,TEMPLATE,CONFIG,SUBCFG,OUTFILE 
+
+  if len(ACTION) == 0:
+    print(MANUAL)
+    print("Error: wrong command-line prameters !")
+
+  if "prepare" in ACTION:
+    cfg={}
+  
+    if SUBCFG in CONFIG.keys():
+      cfg.update(CONFIG[SUBCFG])
+    if "common" in CONFIG.keys():
+      cfg.update(CONFIG["common"])
+    if SUBCFG == "":
+      cfg = CONFIG
+  
+    output = prepareConfig(TEMPLATE,cfg)
+    writeFile(OUTFILE,output)
+
+  if "list" in ACTION:
+    listSubConfig(CONFIG)
+    
+
+####################################################################### }}} 1
+## COMMAND-LINE ####################################################### {{{ 1
+
+def commandLine(args=sys.argv):
+  global ACTION,TEMPLATE,CONFIG,SUBCFG,OUTFILE 
+
+  argx=args.pop(0)
+  if not len(args):
+    print MANUAL
+    exit()
+
+  while len(args):
+    argx=args.pop(0)
+    if argx in ("-t","--template"): 
+      TEMPLATE=loadFile(args.pop(0))
+      ACTION.append("prepare")
+      continue
+    if argx in ("-c","--config"):
+      CONFIG=loadJson(args.pop(0))
+      continue
+    if argx in ("-s","--part","--site","--sub"):
+      SUBCFG=args.pop(0)
+      continue
+    if argx in ("-o","--output","-out"):
+      OUTFILE=args.pop(0)
+      continue
+    if argx in ("-l","--list"):
+      ACTION.append("list")
+      continue
+    else:
+      print("Error: wrong parameter %s" % (argx))
+      exit()
+    
+
+####################################################################### }}} 1
+## MAIN ############################################################### {{{ 1
+
+if __name__ == "__main__":
+  commandLine()
+  takeAction()
+
+
+####################################################################### }}} 1
+# --- end ---
