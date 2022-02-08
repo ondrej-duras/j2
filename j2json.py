3bc18e514d496acb118d80bd951d07f2296b1a18
Ing. Ondrej ĎURÁS (dury) <ondrej-duras@users.noreply.github.com>
2022-02-08 05:00:16 +0100

    humanized .json added
    It's able to read commented .json files.
    ... comments are not allowed in .JSON, but who should remember an usage of so many variables ???
    The second: if some syntax error occur, then j2json writes the line number where to find it.

--- j2json.py
+++ j2json.py
@@ -6,7 +6,7 @@
 
 ## MANUAL ############################################################# {{{ 1
 
-VERSION = "2021.111703"
+VERSION = "2022.020701"
 MANUAL  = """
 NAME: J2JSON Jinja2 Template + JSON Config = Configuration Ticket
 FILE: j2json.py
@@ -18,8 +18,8 @@ DESCRIPTION:
 
   Script needs two things on input:
    - simple Jinja2 template
-   - json configuration
-
+   - json configuration (humanized .json works too)
+  Humanized (.juh as extension) uses # comments
 
 USAGE:
   j2json.py -t TEMPLATE.j2 -c CONFIG.json -o CONFIG-TICKET.txt
@@ -50,12 +50,55 @@ PARAMETERS:
     -b  --batch    - prepares a batch for masive action
     -f1 -bra       - adds brackets to keys "{{key}}" /explicit
     -f0 -no        - suppress brackets in keys "key" /default
+    -h  --help     - this help
+    -h2 --help2    - .json input file format - file example
 
 SEE ALSO:
   https://github.com/ondrej-duras/
 
 VERSION: %s GPLv2
 """ % (VERSION)
+
+
+MANUAL2 = """
+# common section, included within all other sections
+{
+"common" : {
+ "_TEMPLATE_URL": "https://straka.lab.orange.sk/templates/%", # link to templates
+ "_CRQ": "CRQ000000967001", # ticket of configuration change
+ "_CRQ_SUBJECT": "X-345-BA: STP_SIGTRAN for TestBed1",  # subject of ticket
+ "site_id" : "X-345-BA",
+ "bgp_as_osk" : "15962",
+ "bgp_as_core": "65111" 
+},
+
+"c11-vpn-pri" : {
+  "_TEMPLATE": "01-VPN_BGP.j2",
+  "_OUTPUT":   "c11-VPN_BGP-SIGTRAN_PRI-CRQprocedure.txt",
+  "vpn": "SIGTRAN_PRI",
+  "target":       "15962:1000111", 
+  "primary_rd":   "<IPSA_RD1_PRI>",
+  "secondary_rd": "<IPSA_RD2_PRI>", 
+  "local_preference_gw1": "200",
+  "local_preference_gw2": "20"
+},
+"c56-foreign-sec2ims": {
+  "_TEMPLATE": "05-FOREIGN.j2",
+  "_OUTPUT":   "c56-FOREIGN-STP_SIG_SEC_T2IMS_SIGNALING_PRI-CRQprocedure.txt",
+  "vpn": "IMS_SIGNALING_PRI",
+  "core_vrf": "CORE_STP_SIG_SEC_T", 
+  "uplink_vlan_id": "3095", 
+  "uplink_vlan_name": "STP_SIG_SEC_T2IMS_SIGNALING_PRI", 
+  "uplink_ip4": "172.17.111.1",
+  "foreign_ipnet_m": [   # this kind assures multiple/multiline application.
+    "172.16.77.185/32",  # Line, containing foreign_ipnet_m is applied 3 times.
+    "172.16.77.190/32",  # each one with other value
+    "172.16.77.225/32"
+  ]
+}
+}
+"""
+
 
 
 ####################################################################### }}} 1
@@ -159,12 +202,34 @@ def mergeDict(x,y):
    z.update(y)
    return z
 
-# loads a .json file
+# # loads a .json file
+# def loadJson(fname):
+#   fh=open(fname,"r")
+#   output = json.load(fh)
+#   fh.close()
+#   return output
+
+# loads humanized .json
+# "humanized" means it may contain some comments
 def loadJson(fname):
-  fh=open(fname,"r")
-  output = json.load(fh)
+  fh = open(fname,"r")
+  text1 = fh.read()
+  text2 = ""
   fh.close()
+  for line in text1.splitlines():
+    if re.match(r"\s*#",line): continue
+    line = re.sub(r'#[^"]+$','',line)
+    text2 += line + "\n"
+  #print(text2)
+  output = {}
+  try:
+    output = json.loads(text2)
+  except ValueError as err:
+    print(err)  # this exception
+    exit()      # shows where the error is
   return output
+
+
 
 
 def loadFile(fname):
@@ -243,6 +308,12 @@ def takeAction():
     print(MANUAL)
     print("Error: wrong command-line prameters !")
 
+  if "help" in ACTION:
+    print(MANUAL)
+
+  if "help2" in ACTION:
+    print(MANUAL2)
+
   # prepares the particular sub/configuration
   if "prepare" in ACTION:
     cfg={}
@@ -285,6 +356,12 @@ def commandLine(args=sys.argv):
 
   while len(args):
     argx=args.pop(0)
+    if argx in ("-h","--help","-?"):
+      ACTION.append("help")
+      continue 
+    if argx in ("-h2","--help2","-?2"):
+      ACTION.append("help2")
+      continue 
     if argx in ("-t","--template"): 
       FN_TEMPLATE=args.pop(0)
       TEMPLATE=loadFile(FN_TEMPLATE)
